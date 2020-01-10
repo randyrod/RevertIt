6215165914b2e5c244d1fc51f86b830d7d142256
Randy Collado <randyro91@gmail.com>
2020-01-10 14:58:33 +0100

    Init functions

--- RevertIt/Program.cs
+++ RevertIt/Program.cs
@@ -6,7 +6,27 @@ namespace RevertIt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var reverseMe = new [] { 5, 7, 8, 10, 11 };
+            Console.WriteLine($"Array before reverse");
+            PrintArray(reverseMe);
+
+            ReverseArray(reverseMe);
+
+            Console.WriteLine($"Array after reverse");
+            PrintArray(reverseMe);
+        }
+
+        private static void ReverseArray(int[] arr)
+        {
+            // Implement arr.Reverse()
+        }
+
+        private static void PrintArray(int[] arr)
+        {
+            foreach (var i in arr)
+            {
+                Console.Write($"{i} ");
+            }
         }
     }
 }
